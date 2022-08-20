ad7e72fe516a3d7dbc4456019e322d77c73e5c04
Zach <z.d.jorgensen@gmail.com>
2022-08-20 02:39:01 -0600

    Employee object create/Employee test update

--- tests/Employee.test.js
+++ tests/Employee.test.js
@@ -3,47 +3,48 @@ const Employee = require("../lib/Employee");
 describe("Employee", () => {
     // Test for all use cases when initializing a new Employee object
     describe('Initialization', () => {
-        it('should create an object with a name, id, and email')
-            const Employee = new Employee('Zach', 1, 'z.d.jorgensen@gmail.com');
+        it('should create an object with a name, id, and email', () => {
+            const employee = new Employee('Zach', 1, 'z.d.jorgensen@gmail.com');
     
             // Verify that new object has correct properties
-            expect(Employee.name).toEqual('Zach');
-            expect(Employee.id).toEqual(1);
-            expect(Employee.email).toEqual('z.d.jorgensen@gmail.com')
+            expect(employee.name).toEqual('Zach');
+            expect(employee.id).toEqual(1);
+            expect(employee.email).toEqual('z.d.jorgensen@gmail.com')
+        }); 
+
+        // Verifies new object has correct paramaters
+        it('should throw an error if provided no arguments', () => {
+            const cb = () => new Employee();
+    
+            const err = new Error("Expected parameter 'name' to be a non-empty string");
+            expect(cb).toThrowError(err);
+        });
+    
+        // Verifies new object has correct paramaters
+        it('should throw an error if not provided a name', () => {
+            const cb = () => new Employee(1, 'z.d.jorgensen@gmail.com');
+    
+            const err = new Error("Expected parameter 'name' to be a non-empty string");
+    
+            expect(cb).toThrowError(err);
+        }); 
+    
+        // Verifies new object has correct paramaters
+        it('should throw an error if not provided an id', () => {
+            const cb = () => new Employee('Zach', 'z.d.jorgensen@gmail.com');
+    
+            const err = new Error("Expected parameter 'id' to be a number");
+    
+            expect(cb).toThrowError(err);
+        });
+    
+        // Verifies new object has correct paramaters
+        it('should throw an error if not provided an email', () => {
+            const cb = () => new Employee('Zach', 1);
+    
+            const err = new Error("Expected parameter 'email' to be a non-empty string");
+    
+            expect(cb).toThrowError(err);
+        }); 
     });
-
-    // Verifies new object has correct paramaters
-    it('should throw an error if provided no arguments', () => {
-        const cb = () => new Employee();
-
-        const err = new Error("Needs to be passed parameters")
-        expect(cb).toThrowError(err);
-    });
-
-    // Verifies new object has correct paramaters
-    it('should throw an error if not provided a name', () => {
-        const cb = () => new Employee(1, 'z.d.jorgensen@gmail.com');
-
-        const err = new Error("Expected parameter 'name'");
-
-        expect(cb).toThrowError(err);
-    }); 
-
-    // Verifies new object has correct paramaters
-    it('should throw an error if not provided an id', () => {
-        const cb = () => new Employee('Zach', 'z.d.jorgensen@gmail.com');
-
-        const err = new Error("Expected parameter 'id'");
-
-        expect(cb).toThrowError(err);
-    });
-
-    // Verifies new object has correct paramaters
-    it('should throw an error if not provided an email', () => {
-        const cb = () => new Employee('Zach', 1);
-
-        const err = new Error("Expected parameter 'email'");
-
-        expect(cb).toThrowError(err);
-    }); 
 });
